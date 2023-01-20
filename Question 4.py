9deee93d18e5fa947bd1da3c5d52e3d6f1b89ba3
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-20 09:15:11 +0530

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -1,4 +1,4 @@
 #Determining the number of candies
-for candies in range(1,200):
-    if candies%5==2 and candies%6==3 and candies%7==2:
+for candies in range(1,200):                             #running a loop for 200 candies
+    if candies%5==2 and candies%6==3 and candies%7==2:   #If the number satisfies the condition of remainder
         print("The number of candies are",candies)
