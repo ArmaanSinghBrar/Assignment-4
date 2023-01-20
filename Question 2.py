4c4a5c8fbad19e90379c71060a07f9e38cb97203
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-20 09:08:55 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,10 +1,10 @@
 #Program to tell if a year is a leap or not a leap year
 year = int(input("Enter a year :"))
-if year%400==0:
+if year%400==0:                                    #If divisible by 400 it is a leap year
     print("The given year is a leap year")
-elif year%100==0:
+elif year%100==0:                                  #If not divisible by 400 but divisible by 100 it is not a leap year
     print("The given year is not a leap year")
-elif year%4==0:
+elif year%4==0:                                    #If not divisible by 400 and 100 but divisible by 4 it is a leap year
     print("The given year is a leap year")
 else:
     print("The given year is not a leap year")
